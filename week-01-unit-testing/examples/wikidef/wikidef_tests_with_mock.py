4b829b6cc01c008177bfe98f3a38790b1330d9e5
Joseph Sheedy <joseph.sheedy@gmail.com>
2014-09-09 16:49:43 -0700

    updates

--- week-01-unit-testing/examples/wikidef/wikidef_tests_with_mock.py
+++ week-01-unit-testing/examples/wikidef/wikidef_tests_with_mock.py
@@ -13,13 +13,13 @@ class WikiDefTest(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_article_success(self):
-        article = Definitions.article("Robot")        
-        self.assertIn("mechanical", article)
+    # def test_article_success(self):
+    #     article = Definitions.article("Robot")        
+    #     self.assertIn("mechanical", article)
 
-    def test_missing_article_failure(self):
-        missing_article_title = "!!!!!-NonExistentArticle"
-        self.assertRaises(ParseError, Definitions.article, missing_article_title)
+    # def test_missing_article_failure(self):
+    #     missing_article_title = "!!!!!-NonExistentArticle"
+    #     self.assertRaises(ParseError, Definitions.article, missing_article_title)
 
     # patch with a decorator
     @patch('definitions.Wikipedia.article')
