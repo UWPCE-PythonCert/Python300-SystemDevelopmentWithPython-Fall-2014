271affee91b8be1de6ddf7116a73fc22c171775e
Joseph Sheedy <joseph.sheedy@gmail.com>
2014-10-07 15:22:24 -0700

    updates

--- week-05-serialization/examples/mutable.py
+++ week-05-serialization/examples/mutable.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
+"""mutable kwarg failure demo"""
+
 def add_plus_one(values=[]):
     """adds the values in values together, plus 1"""
     values.append(1)
     return sum(values)
 
 print add_plus_one()
-print add_plus_one([1,2,3])
+print add_plus_one([1, 2, 3])
 print add_plus_one()
