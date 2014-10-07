7c5c2dce8ed7da097aca5c659a408f1cdfc67914
Joseph Sheedy <joseph.sheedy@gmail.com>
2014-10-07 16:44:58 -0700

    udpates

--- week-05-serialization/examples/badpicklemodule.py
+++ week-05-serialization/examples/badpicklemodule.py
@@ -1,4 +1,8 @@
 
+# this function can not be pickled due to the requirement:
+# - "functions defined at the top level of a module"
+# see https://docs.python.org/2/library/pickle.html#what-can-be-pickled-and-unpickled
+
 def foo():
     def bar():
         return 2
