b83019b1b9dfe4ac2d45dea9e3a9ba6a2f7443c8
Joseph Sheedy <joseph.sheedy@gmail.com>
2014-10-28 15:11:14 -0700

    updates

--- week-08-profiling-and-tuning/examples/numpy/images.py
+++ week-08-profiling-and-tuning/examples/numpy/images.py
@@ -3,15 +3,20 @@
 import numpy as np
 from PIL import Image
 
-# add black bands to image, for fun
 image = Image.open('test_image.jpg')
 image_array = np.asarray(image)
 image_array.setflags(write=True)
+
 # glitch data
 image_array -= 200
 w,h,channels = image_array.shape
-for x in xrange(0,w,2):
-    image_array[:][x][:] = 0
 
-image2 = Image.fromarray(image_array)
-image2.save('out.png')
+# insert black bands
+for x in xrange(0,h,20):
+    image_array[:, x:x+5, :] = 0
+
+# swap square slices of image
+for i in xrange(0, w-20, 20):
+	for j in xrange(0, h-20, 20):
+		image_array[i:i+10,j:j+10,:] = image_array[i+10:i+20, j+10:j+20,:]
+Image.fromarray(image_array).save('out.png')
--- week-08-profiling-and-tuning/examples/numpy/matrix.py
+++ week-08-profiling-and-tuning/examples/numpy/matrix.py
@@ -1,26 +1,33 @@
 import numpy
+import timeit
 
 from decorators import timer
 
 @timer
 def offset(matrix, x):
-    """add x to all values in matix"""
     for i in xrange(len(matrix)):
         for j in xrange(len(matrix[0])):
             matrix[i][j] += x
 
 @timer
 def numpy_offset(matrix, x):
-    matrix += x
+	"""add x to all values in matix"""
+	matrix += x
 
-N=3000
+# create a 2D NxN matrix
+python_data_setup = "\
+from __main__ import offset;\
+N=3000;\
+python_data = [[x for x in xrange(N)] for y in xrange(N) ]"
 
-data = [[x for x in xrange(N)] for y in xrange(N) ]
+numpy_data_setup = "\
+import numpy;\
+from __main__ import numpy_offset;\
+N=3000;\
+numpy_data = numpy.array([[x for x in xrange(N)] for y in xrange(N) ])"
 
-numpy_data = numpy.array(data)
+pure_python_statement = "offset(python_data, 10)"
+numpy_statement = "numpy_offset(numpy_data, 10)"
+print "Pure python: %fs" % timeit.timeit(pure_python_statement, setup=python_data_setup, number=3)
+print "Numpy: %fs" % timeit.timeit(numpy_statement, setup=numpy_data_setup, number=3)
 
-offset(data, 10)
-numpy_offset(numpy_data, 10)
-
-print data[4][4]
-print numpy_data[4][4]
